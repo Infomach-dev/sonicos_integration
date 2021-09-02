19650992664b5cbba97b555c79050448ae6cb071
joaovmlima <oi.oaoj@gmail.com>
2021-09-02 17:26:57 -0300

    update port variable

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("routes.appRoutes:app", host="0.0.0.0", port=os.PORT, reload=True)
+    uvicorn.run("routes.appRoutes:app", host="0.0.0.0", port=os.environ.get('PORT'), reload=True)
 
 
