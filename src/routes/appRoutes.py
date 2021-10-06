de85806533286f3622af9b116903cc5652c89cad
joaovmlima <oi.oaoj@gmail.com>
2021-10-06 12:58:30 -0300

    add sessions

--- src/routes/appRoutes.py
+++ src/routes/appRoutes.py
@@ -1,3 +1,5 @@
+from pymongo import response
+import requests
 import tldextract
 from db import main as db
 from argon2 import PasswordHasher
@@ -10,11 +12,104 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.responses import PlainTextResponse, RedirectResponse
 
+from pydantic import BaseModel
+from fastapi import HTTPException, FastAPI, Response, Depends
+from uuid import UUID, uuid4
+
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+
+
+class SessionData(BaseModel):
+    username: str
+
+
+cookie_params = CookieParameters()
+
+# Uses UUID
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE",
+    cookie_params=cookie_params,
+)
+backend = InMemoryBackend[UUID, SessionData]()
+
+
+class BasicVerifier(SessionVerifier[UUID, SessionData]):
+    def __init__(
+        self,
+        *,
+        identifier: str,
+        auto_error: bool,
+        backend: InMemoryBackend[UUID, SessionData],
+        auth_http_exception: HTTPException,
+    ):
+        self._identifier = identifier
+        self._auto_error = auto_error
+        self._backend = backend
+        self._auth_http_exception = auth_http_exception
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def auto_error(self):
+        return self._auto_error
+
+    @property
+    def auth_http_exception(self):
+        return self._auth_http_exception
+
+    def verify_session(self, model: SessionData) -> bool:
+        """If the session exists, it is valid"""
+        return True
+
+
+verifier = BasicVerifier(
+    identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+)
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="views")
+
+@app.post("/create_session/{name}")
+async def create_session(name: str, response: Response):
+
+    session = uuid4()
+    data = SessionData(username=name)
+
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
+
+    return f"created session for {name}"
+
+
+@app.get("/whoami", dependencies=[Depends(cookie)])
+async def whoami(session_data: SessionData = Depends(verifier)):
+    return session_data
+
+
+@app.post("/delete_session")
+async def del_session(response: Response, session_id: UUID = Depends(cookie)):
+    await backend.delete(session_id)
+    cookie.delete_from_response(response)
+    return "deleted session"
+
+# -------------------------------------------------------------------------------------------------------
 
 @app.get("/")
 def index(request: Request):
@@ -39,17 +134,31 @@ def showList(request: Request, name: str = Path(...)):
         return templates.TemplateResponse("show_uri_list.html", {"request": request, "uriLists": response})
 
 @app.post("/login")
-def loginToPortal(request: Request, username: str = Form(...), password: str = Form(...)):
+async def loginToPortal(request: Request, response: Response, username: str = Form(...), password: str = Form(...)):
     searchUser = db.usersCollection.find_one({'username': username})
+
+    async def create_session(name: str, response: Response):
+        session = uuid4()
+        data = SessionData(username=name)
+
+        await backend.create(session, data)
+        cookie.attach_to_response(response, session)
+
+        return f"created session for {name}"
+
     if not searchUser:
         return PlainTextResponse("Usuário não encontrado!")
     
     hasher = PasswordHasher()
     try: 
         hasher.verify(searchUser['password'], password)
-        return RedirectResponse("/portal", status_code=303)
     except:
         return PlainTextResponse("Usuário ou senha incorreta")
+
+    await create_session(username, response)
+    return RedirectResponse("/portal", status_code=301, headers=response.headers)
+    # return PlainTextResponse(f"Sessão criada para {username}!")
+
 
 @app.post("/fwlogin")
 def loginToAPI(request: Request, _id: str = Form(...)):
@@ -142,5 +251,5 @@ def preemptMode(request: Request):
 @app.get("/portal")
 def portal(request: Request):
     fwList = db.firewallsCollection.find({'companyID': '1'})
-    print(fwList)
+
     return templates.TemplateResponse("portal.html", {"request": request, "fwList": fwList})
